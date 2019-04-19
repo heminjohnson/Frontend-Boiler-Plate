af8ee2c8e507592589a571e3d1ce30ca79bdc610
Hemin Johnson <achayan1992@gmail.com>
2019-04-19 09:30:06 +0200

    ci: configure routes and some dummy components

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+const Header = () => <div>Header</div>
+const Landing = () => <div>Landing</div>
+const dashboard = () => <div>DashBoard</div>
+const dashboardTwo = () => <div>DashBoardTwo</div>
 
 const App = () => {
   return (
     <div>
-      Hello World
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path='/' component={Landing} />
+          <Route exact path='/one' component={dashboard} />
+          <Route path='/two' component={dashboardTwo} />
+        </div>
+      </BrowserRouter>
     </div>
   )
 }
